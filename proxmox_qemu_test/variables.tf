651c49655a102b2a7321551b21556f614d29f9fd
Tomo-9925 <t.okada0325@gmail.com>
2023-01-30 00:00:00 +0900

    fix sensitive variables in proxmox vm qemu test

--- proxmox_qemu_test/variables.tf
+++ proxmox_qemu_test/variables.tf
@@ -1,19 +1,23 @@
 variable "PM_API_URL" {
   type        = string
   description = "Value of Proxmox API URL"
+  sensitive   = true
 }
 
 variable "PM_USER" {
   type        = string
   description = "Value of Proxmox VM create user name"
+  sensitive   = true
 }
 
 variable "PM_PASS" {
   type        = string
   description = "Value of Proxmox VM create user password"
+  sensitive   = true
 }
 
 variable "SSH_PUB_KEYS" {
-  type = string
+  type         = string
   desicription = "Value of SSH Public keys"
+  sensitive    = true
 }
